23aa46ccdb04f25c6234c71bf89a1fc67f98b1d8
unknown <moroccoofims@gmail.com>
2023-02-04 14:24:55 +0100

    exemple

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-md navbar-dark  ">
-                <Link to="/" className="navbar-brand  m-auto " href="#"><img src={siteLogo} className='logo ' alt='site-logo' /></Link>
+                <Link to="/AnimeWorld" className="navbar-brand  m-auto " href="#"><img src={siteLogo} className='logo ' alt='site-logo' /></Link>
             </nav>
         </>
     )
